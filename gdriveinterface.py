860ef694eb5da07b3095f6e8828150bd758f9d25
Andrew Loeber <98239914+andrew-loeber@users.noreply.github.com>
2023-07-06 13:33:26 -0700

    Initial commit

---
+++ gdriveinterface.py
@@ -0,0 +1,40 @@
+import proj_ref
+from google.colab import auth
+import requests
+import os
+
+
+def get_account():
+    auth.authenticate_user()
+    gcloud_token = !gcloud auth print-access-token
+    gcloud_tokeninfo = requests.get(
+        'https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=' + gcloud_token[0]
+    ).json()
+    return gcloud_tokeninfo['email'], gcloud_tokeninfo['email'].split('@')[0]
+    
+    
+class GDriveInterface:
+    
+    def __init__(self):
+        self.email, self.account = get_account()
+        self.shared_folder_path = self.get_shared_folder_path()
+        
+    def get_shared_folder_path(self):
+        return proj_ref.shared_folder[self.account]
+        
+    def get_file_path(self, key):
+        path_end = proj_ref.files[key]
+        return os.path.join(self.shared_folder_path, path_end)
+        
+    def get_dir_path(self, key):
+        path_end = proj_ref.dirs[key]['path']
+        return os.path.join(self.shared_folder_path, path_end)
+        
+    def get_sample_path(self, dir_key, file_key):
+        path_list = []
+        path_list.append(self.shared_folder_path)
+        path_list.append(proj_ref.dirs[key]['path'])
+        path_list.append(file_key)
+        path_list.append(proj_ref.dirs[key]['file_ext'])
+        return os.path.join(*path_list)
+        
---
+++ proj_ref.py
@@ -0,0 +1,52 @@
+shared_folder = {
+    'aloeber': '/content/drive/MyDrive/207-Project'    
+    , 'aminaalavi': '/content/drive/MyDrive/207-Project'
+    , 'hamsini.sankaran': '/content/drive/MyDrive/UCB-MIDS/SEM-2/MACHINE-LEARNING-207/207-Project'
+    , 'rachelgao': '/content/drive/MyDrive/207/207-Project'
+}
+
+files = {
+    'taxonomy':             '/BirdCLEF/eBird_Taxonomy_v2021.csv'
+    , 'orig_metadata':      '/BirdCLEF/train_metadata.csv'
+    , 'sample_metadata':    '/data/sample_metadata.csv'
+    , 'species_metadata':   '/data/species_metadata.csv'
+    , 'train_metadata':     '/data/train_metadata.csv'
+    , 'test_metadata':      '/data/test_metadata.csv'
+}
+
+dirs = {
+    'orig_audio': {
+        'path':       '/BirdCLEF/train_audio'
+        , 'file_ext': '.ogg'
+    }
+    
+    , 'train_audio': {
+        'path':       '/data/train/audio_files'
+        , 'file_ext': '.ogg'
+    }
+    
+    , 'test_audio': {
+        'path':       '/data/test/audio_files'
+        , 'file_ext': '.ogg'
+    }
+    
+    , 'train_npy_full': {
+        'path':       '/data/train/librosa_loaded'
+        , 'file_ext': '.npy'
+    }
+    
+    , 'test_npy_full': {
+        'path':       '/data/test/librosa_loaded'
+        , 'file_ext': '.npy'
+    }
+    
+    , 'train_npy_loud5s': {
+        'path':       '/data/train/librosa_loaded_loudest_5sec'
+        , 'file_ext': '.npy'
+    }
+    
+    , 'test_npy_loud5s': {
+        'path':       '/data/test/librosa_loaded_loudest_5sec'
+        , 'file_ext': '.npy'
+    }
+}
