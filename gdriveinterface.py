28e81b5b4e201549549fedd02a9e1b1bdc30a96b
Andrew Loeber <98239914+andrew-loeber@users.noreply.github.com>
2023-07-06 14:07:48 -0700

    Fix pathing issue with file extensions in gdriveinterface.py

--- gdriveinterface.py
+++ gdriveinterface.py
@@ -31,7 +31,6 @@ class GDriveInterface:
         path_list = []
         path_list.append(self.shared_folder_path)
         path_list.append(proj_ref.dirs[dir_key]['path'])
-        path_list.append(file_key)
-        path_list.append(proj_ref.dirs[dir_key]['file_ext'])
+        path_list.append(file_key + proj_ref.dirs[dir_key]['file_ext'])
         return os.path.join(*path_list)
         
